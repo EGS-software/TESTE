cf62963d7aef19462fac77efc3a0bd855876350d
JaoVicy <filipimjoa@gmail.com>
2025-04-28 14:07:26 -0300

    Initial Commit

---
+++ TESTETests/TESTETests.swift
@@ -0,0 +1,17 @@
+//
+//  TESTETests.swift
+//  TESTETests
+//
+//  Created by João Víctor Benetti Filipim on 28/04/25.
+//
+
+import Testing
+@testable import TESTE
+
+struct TESTETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
